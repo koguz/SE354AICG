e2d5336f20a7c7b28c16357f355a05b8db52d1b6
Kaya Oğuz <kayaoguz@gmail.com>
2015-01-11 10:01:11 +0200

    bug fix on collision
    the power calculation corrected. keep the max value of power at 1000.

--- Assets/lecture02/CollisionAvoidance.cs
+++ Assets/lecture02/CollisionAvoidance.cs
@@ -2,20 +2,24 @@
 using System.Collections;
 
 public class CollisionAvoidance : MonoBehaviour {
-
-	private DynamicSeek seek;
+	
+	private DynamicArrive arrive;
 	private DynamicAlign align;
+	private GameObject k;
 	public Vector3 target = new Vector3(0, 0, 10);
 	private int width = 1; // We know that width of cube is 1. 
 
 	// Use this for initialization
 	void Start () {
-		seek = GetComponent<DynamicSeek>();
+		arrive = GetComponent<DynamicArrive>();
 		align= GetComponent<DynamicAlign>();
+		k = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		k.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// if( (target-transform.position).magnitude < 0.2 ) return;
 		/* We have an original target, but as we move along we 
 		 * will avoid obstacles. 
 		 * To do so, we will "evade" the obstacles by temporarily
@@ -23,16 +27,18 @@ public class CollisionAvoidance : MonoBehaviour {
 		 * */
 		RaycastHit hit;
 		/* Check as far as twice our speed. */
-		float distanceToCheck = seek.velocity.magnitude * 2;
+		float distanceToCheck = arrive.velocity.magnitude * 2;
 
 		Vector3 tempTarget = new Vector3(0, 0, 0);
 		if(Physics.SphereCast(transform.position, width, transform.forward, out hit, distanceToCheck)) {
 			// Debug.Log ("hit!");
 			/* Now that we know there is something ahead. */
-			tempTarget = (distanceToCheck/hit.distance) * (transform.position - hit.transform.position);
+			float power = distanceToCheck / hit.distance; // can go to infinity!
+			if(power > 1000) power = 1000;
+			tempTarget = power * (transform.position - hit.transform.position);
 		}
-
-		seek.target = target + tempTarget;
-		align.target = Mathf.Atan2 (seek.velocity.x, seek.velocity.z) * Mathf.Rad2Deg;
+		arrive.target = target + tempTarget;
+		k.transform.position = target + tempTarget;
+		align.target = Mathf.Atan2 (arrive.velocity.x, arrive.velocity.z) * Mathf.Rad2Deg;
 	}
 }
