e7948bedcf0c38c76b41bb3e8d86f52a512b23ed
Kaya Oğuz <kayaoguz@gmail.com>
2018-02-26 22:04:43 +0300

    Basic Group Movement
    A very basic group movement with fixed positions. Needs a lot of work...
    Implemented during the lecture on 2018.02.26...

---
+++ Assets/ai/01_movement/FixedCameraGroupMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCameraGroupMovement : MonoBehaviour {
+
+	private ArrayList entities;
+	// Use this for initialization
+	void Start () {
+		entities = new ArrayList ();
+		foreach (GameObject g in GameObject.FindGameObjectsWithTag("gr"))
+			entities.Add (g);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			RaycastHit h;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+			if (Physics.Raycast (ray, out h)) {
+				// Debug.Log (h.point);
+				// Debug.DrawRay (ray.origin, ray.direction * 100, Color.yellow);
+				Vector3 target = h.point;
+				target.y = 0.5f; // let's ignore y 
+				// Change KArrive to DArrive for dynamic version.
+				// GameObject.Find("Entity").GetComponent<KArrive>().target = target;
+				int v = -3;
+				foreach (GameObject g in entities) {
+					g.GetComponent<KArrive> ().target = target + (new Vector3 (0, 0, v));
+					v += 2;
+				}
+			}
+
+		}
+	}
+}
